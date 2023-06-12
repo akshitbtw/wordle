e01c735738a584ed0f6f8588f2545beb8e43701a
Akshit <akshit.40@gmail.com>
2023-06-13 03:01:07 +0530

    modal transition added

--- modal.js
+++ modal.js
@@ -1,13 +1,17 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const modal = document.querySelector('.modal');
     const openBtn = document.querySelector('.openBtn');
     const closeBtn = document.querySelector('.closeBtn');
 
-    openBtn.addEventListener('click', ()=>{
+    openBtn.addEventListener('click', () => {
         modal.showModal();
     });
 
-    closeBtn.addEventListener('click', ()=>{
-        modal.close();
+    closeBtn.addEventListener('click', () => {
+        modal.setAttribute('closing', "");
+        modal.addEventListener('animationend', () => {
+            modal.removeAttribute('closing');
+            modal.close();
+        }, { once: true });
     });
 });
